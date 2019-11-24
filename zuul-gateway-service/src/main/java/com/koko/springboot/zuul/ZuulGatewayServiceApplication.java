831c595d87fdf62c37fc8b6a501f8bc3841842d1
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2019-11-24 11:47:32 +0530

    zuul gateway service

---
+++ zuul-gateway-service/src/main/java/com/koko/springboot/zuul/ZuulGatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.koko.springboot.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayServiceApplication.class, args);
+	}
+
+}
